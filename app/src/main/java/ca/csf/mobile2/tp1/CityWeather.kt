60d8e6bc78663c62a7537c11d93b0b82f6a24e26
Ludovic Garon <ludovicgaron54@gmail.com>
2020-01-14 12:17:50 -0500

    initial commit started ui

---
+++ app/src/main/java/ca/csf/mobile2/tp1/CityWeather.kt
@@ -0,0 +1,13 @@
+package ca.csf.mobile2.tp1
+
+import org.json.JSONObject
+
+class CityWeather(){
+
+    var city: String = ""
+    var type: String = ""
+    var temperatureInCelsius: Int = 0
+
+
+
+}
